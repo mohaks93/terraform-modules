9a54c4df0b270faf6235ba6d75d8ef5016268217
MohammedLakhdar <lakhdar.mo@gmail.com>
2023-09-30 18:34:45 +0200

    recommit

--- route-53/variables.tf
+++ route-53/variables.tf
@@ -1,5 +1,5 @@
 # route-53 variables
 variable "domain_name" {}
 variable "record_name" {}
-variable "var.application_load_balancer_dns_name" {}
-variable "var.application_load_balancer_zone_id" {}
+variable "application_load_balancer_dns_name" {}
+variable "application_load_balancer_zone_id" {}
